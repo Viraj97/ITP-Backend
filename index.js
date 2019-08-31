e89ce9db3c3042d4b80f050acd4c82f166376f44
supun <supunharsha12r@gmail.com>
2019-08-31 15:28:05 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const api1 = require('./api1');
 const login = require('./login');
 const newcustomer = require('./files/Customer/newcustomer');
 // const dbconnection = require('./dbconnection')
-const deletecustomer = require('./files/Customer/deletecustomer')
+const deletecustomer = require('./files/Customer/deletecustomer');
+const addstockdetails = require('./files/Restaurent/addstockdetails');
+const deletedetails = require('./files/Restaurent/deletedetails');
 var express = require('express');
 
 const app = express();
@@ -12,5 +14,7 @@ login(app);
 // dbconnection(app);
 newcustomer(app);
 deletecustomer(app);
+addstockdetails(app);
+deletedetails(app);
 
 app.listen(3000);
