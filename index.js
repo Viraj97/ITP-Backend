02257551193e9f09a61a1989af9a31b2b02445e6
supun <supunharsha12r@gmail.com>
2019-09-03 00:19:58 +0530

    Updated ITP Back End

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const newcustomer = require('./files/Customer/newcustomer');
 const deletecustomer = require('./files/Customer/deletecustomer');
 const addstockdetails = require('./files/Restaurent/addstockdetails');
 const deletedetails = require('./files/Restaurent/deletedetails');
+const tablebooking = require('./files/Restaurent/tablebooking');
+const tablebookingdelete = require('./files/Restaurent/tablebookingdelete');
 var express = require('express');
 
 const app = express();
@@ -16,5 +18,6 @@ newcustomer(app);
 deletecustomer(app);
 addstockdetails(app);
 deletedetails(app);
-
+tablebooking(app);
+tablebookingdelete(app);
 app.listen(3000);
